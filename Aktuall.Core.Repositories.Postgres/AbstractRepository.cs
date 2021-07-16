242427c925d63aa2b395f12822a53e6b1bb3bb97
Isakov Anatolii <a.isakov@skbkontur.ru>
2021-07-16 22:55:41 +0500

    new: minimum needed logic + CI

---
+++ Aktuall.Core.Repositories.Postgres/AbstractRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aktuall.Core.Repositories.Postgres
+{
+    [PublicAPI]
+    public abstract class AbstractRepository<TEntity, TKey> 
+        where TKey : class
+        where TEntity : Entity<TKey>
+    {
+        protected readonly DbContext Context;
+        protected readonly DbSet<TEntity> Set;
+
+        protected AbstractRepository(DbContext context, DbSet<TEntity> set)
+        {
+            Context = context;
+            Set = set;
+        }
+
+        public virtual async Task<TEntity> InsertAsync(TEntity entity)
+        {
+            await Context.AddAsync(entity);
+            await Context.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual async Task<IReadOnlyList<TEntity>> InsertAsync(IEnumerable<TEntity> entities)
+        {
+            var entitiesList = entities.ToList();
+            foreach (var entity in entitiesList)
+            {
+                await Context.AddAsync(entity);
+            }
+
+            await Context.SaveChangesAsync();
+            return entitiesList.ToList();
+        }
+
+        public virtual async Task<TEntity> GetAsync(TKey id)
+        {
+            return await Context.FindAsync<TEntity>(id);
+        }
+
+        public virtual async Task<IReadOnlyList<TEntity>> GetAsync(Func<TEntity, bool> filterFunc)
+        {
+            var entities = await GetAllAsync();
+            return entities.Where(filterFunc).ToList();
+        }
+
+        public virtual async Task RemoveAsync(TEntity entity)
+        {
+            Context.Remove(entity);
+            await Context.SaveChangesAsync();
+        }
+
+        public virtual async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            Context.Update(entity);
+            await Context.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual async Task<IReadOnlyList<TEntity>> GetAllAsync()
+        {
+            return await Set.ToListAsync();
+        }
+    }
+}
---
+++ Aktuall.Core.Repositories.Postgres/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Aktuall.Core.Repositories.Postgres
+{
+    [PublicAPI]
+    public abstract class Entity<T> where T: class
+    {
+        public T Id { get; }
+
+        protected Entity(T id)
+        {
+            Id = id ?? throw new ArgumentNullException(nameof(id), "Entity's id cannot be null");
+        }
+    }
+}
